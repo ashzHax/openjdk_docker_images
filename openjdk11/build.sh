3f2c5b7a0d7ed31f73ec17b04511a62ffec03c80
ashzHax <ashkim2001@gmail.com>
2023-03-20 09:48:06 +0900

    initial commit

---
+++ openjdk11/build.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+IMAGE_NAME="openjdk11"
+CONTAINER_NAME="jdk11"
+SDK_NUM="11"
+MNT_POINT="-v /home/ashz/server/Minecraft/:/home/dkr/mnt/"
+CONTAINER_PORT="-p 25565:25565"
+
+echo "[!] building new openjdk11 image"
+
+echo "[!] deleting old image + build test containers"
+docker rm $CONTAINER_NAME
+docker rmi $IMAGE_NAME
+
+echo "[!] building new image"
+docker build -t $IMAGE_NAME --no-cache .
+
+echo "[!] starting test container"
+docker run -it --name $CONTAINER_NAME $MNT_POINT $CONTAINER_PORT $IMAGE_NAME
+
+echo "[!] finished building new image"
